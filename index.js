7559771008cdf724623c8c30a5f1e3797ef54ee9
Mike Moye <mikemoye14@users.noreply.github.com>
2018-07-21 22:58:39 -0400

    Update index.js

--- index.js
+++ index.js
@@ -14,35 +14,22 @@ server.listen(port, () => {
 // Routing
 app.use(express.static(path.join(__dirname, 'public')));
 
-//users = [];
-
 var socket = require('socket.io')(server);
 
 socket.sockets.on('connection', function (socket) {
-	//socket.broadcast.emit('users', users);
-	
-	//socket.on('init', function (data) {	
-		//socket.emit('init', 'Connected to Dispatch Server');		 
-		//socket.emit('users', users);
-	//});	
-	
-	
+        socket.broadcast.emit('connect', 'Connected to Taxi Dispatch');
 
-	socket.on('order', function (data) {	
-		console.log('new Order from: ' + data);
-		//socket.emit('order', data.order);
-		//socket.broadcast.emit('chatMsg', data.userName + ' says: ' + data.msg);		
-   });
+        socket.on('init', function (data) {
+                socket.emit('init', 'Connected to Dispatch');
+        });
 
-	//socket.on("disconnect", function () {	
-		//users.forEach(function(val, index, users){			
-			//if(val.id == socket.id){			
-				//console.log(val.name + ' disconnected.');
-				//users.splice(index,1);
-				//socket.broadcast.emit('chatMsg', val.userName + ' disconnected.');
-				//socket.broadcast.emit('userDisconnect', val.id);			
-			//}		
-		//});
-	//});
-        
+        socket.on('order', function (data) {
+                console.log('New Order: ' + data);
+                socket.emit('order', 'Order Received');
+        });
+
+        socket.on("disconnect", function () {
+
+        });
+
 });
