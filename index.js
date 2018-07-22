292f3047ca62394e79ea7651d0fd12caaa078de8
Mike Moye <mikemoye14@users.noreply.github.com>
2018-07-21 20:10:53 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ var express = require('express');
 var app = express();
 var path = require('path');
 var server = require('http').createServer(app);
-var io = require('socket.io')(server);
 var port = process.env.PORT || 8080;
 
 server.listen(port, () => {
